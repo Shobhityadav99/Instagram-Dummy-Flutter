afff956ed0744f9eec4b085d3274ddf721c3bef2
Shobhit Yadav <shobhit999000@gmail.com>
2021-02-25 17:21:43 +0530

    Grid View Added

--- lib/screens/Search_Screen.dart
+++ lib/screens/Search_Screen.dart
@@ -13,7 +13,7 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.black,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         actions: <Widget>[
           IconButton(
@@ -22,19 +22,24 @@ class _SearchScreenState extends State<SearchScreen> {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'val',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 3,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(10, (index) {
+          return Center(
+              child: Container(
+            width: MediaQuery.of(context).size.width / 3,
+            height: MediaQuery.of(context).size.height / 5,
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                )),
+          ));
+        }),
       ),
       bottomNavigationBar: BottomNav(),
     );
